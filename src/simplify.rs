38f032ae605dd004c92faf6bfdc701a0d2b73483
Yuheng Su <gipsyh.icu@gmail.com>
2025-01-19 19:42:37 +0800

    update

--- src/simplify.rs
+++ src/simplify.rs
@@ -1,8 +1,8 @@
 use super::Transys;
-use logic_form::{Clause, Lit, LitMap, Var, VarMap};
+use logic_form::{Clause, Lit, Var, VarMap};
 use satif::Satif;
 use satif_minisat::SimpSolver;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 impl Transys {
     fn compress_deps_rec(
@@ -48,6 +48,7 @@ impl Transys {
     }
 
     pub fn simplify(&self, lemmas: &[Clause], keep_dep: bool, assert_constrain: bool) -> Self {
+        assert!(!assert_constrain);
         let mut simp_solver: Box<dyn Satif> = if keep_dep {
             Box::new(SimpSolver::new())
         } else {
@@ -96,74 +97,21 @@ impl Transys {
             domain.insert(*l);
         }
         let dep = Self::compress_deps(self.dependence.clone(), &domain);
-        let mut domain = Vec::from_iter(domain);
-        domain.sort();
-        let mut domain_map = HashMap::new();
-        for (i, d) in domain.iter().enumerate() {
-            domain_map.insert(*d, Var::new(i));
-        }
-        let map_lit = |l: &Lit| Lit::new(domain_map[&l.var()], l.polarity());
-        let inputs = self.inputs.iter().map(|v| domain_map[v]).collect();
-        let latchs: Vec<Var> = self.latchs.iter().map(|v| domain_map[v]).collect();
-        let init = self.init.iter().map(map_lit).collect();
-        let bad = map_lit(&self.bad);
-        let max_latch = domain_map[&self.max_latch];
-        let mut init_map: VarMap<Option<bool>> = VarMap::new_with(max_latch);
-        for l in self.latchs.iter() {
-            init_map[domain_map[l]] = self.init_map[*l];
-        }
-        let constraints = if assert_constrain {
-            Default::default()
-        } else {
-            self.constraints.iter().map(map_lit).collect()
-        };
-        for c in trans.iter_mut() {
-            *c = c.iter().map(map_lit).collect();
-        }
-        let max_var = domain_map[&max_var];
-        let mut next_map = LitMap::new_with(max_var);
-        let mut prev_map = LitMap::new_with(max_var);
-        for l in self.latchs.iter() {
-            let l = l.lit();
-            let p = self.lit_next(l);
-            let l = map_lit(&l);
-            let p = map_lit(&p);
-            next_map[l] = p;
-            next_map[!l] = !p;
-            prev_map[p] = l;
-            prev_map[!p] = !l;
-        }
-        let mut dependence = VarMap::new();
-        for d in domain.iter() {
-            let dep = dep[*d].clone();
-            let dep: Vec<Var> = dep.into_iter().map(|v| domain_map[&v]).collect();
-            dependence.push(dep);
-        }
-        let mut is_latch = VarMap::new_with(max_var);
-        for l in latchs.iter() {
-            is_latch[*l] = true;
-        }
-        let mut restore = HashMap::new();
-        for d in domain.iter() {
-            if let Some(r) = self.restore.get(d) {
-                restore.insert(domain_map[d], *r);
-            }
-        }
         Self {
-            inputs,
-            latchs,
-            init,
-            bad,
-            init_map,
-            constraints,
+            inputs: self.inputs.clone(),
+            latchs: self.latchs.clone(),
+            init: self.init.clone(),
+            bad: self.bad,
+            init_map: self.init_map.clone(),
+            constraints: self.constraints.clone(),
             trans,
             max_var,
-            is_latch,
-            next_map,
-            prev_map,
-            dependence,
-            max_latch,
-            restore,
+            is_latch: self.is_latch.clone(),
+            next_map: self.next_map.clone(),
+            prev_map: self.prev_map.clone(),
+            dependence: dep,
+            max_latch: self.max_latch,
+            restore: self.restore.clone(),
         }
     }
 }
